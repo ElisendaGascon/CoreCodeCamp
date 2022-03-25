2f72e1f20af301fa4716e51822a5cf98b9d09fd4
ElisendaGascon <elisendagascon@icloud.com>
2022-03-25 11:50:57 +0000

    Versioning

--- Startup.cs
+++ Startup.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
+using CoreCodeCamp.Controllers;
 using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.AspNetCore.Mvc.Versioning.Conventions;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -29,6 +32,16 @@ namespace CoreCodeCamp
           opt.AssumeDefaultVersionWhenUnspecified = true;
           opt.DefaultApiVersion = new ApiVersion(1, 1);
           opt.ReportApiVersions = true;
+          opt.ApiVersionReader = new UrlSegmentApiVersionReader();
+          /*          opt.ApiVersionReader = ApiVersionReader.Combine(
+                        new HeaderApiVersionReader("X-Version"),
+                        new QueryStringApiVersionReader("ver", "version"));*/
+
+          opt.Conventions.Controller<TalksController>()
+          .HasApiVersion(1, 0)
+          .HasApiVersion(1, 1)
+          .Action(c => c.Delete(default(string), default(int)))
+            .MapToApiVersion(1, 1);
       });
 
       services.AddControllers();
